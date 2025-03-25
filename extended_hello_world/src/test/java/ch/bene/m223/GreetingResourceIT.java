ffc249fc5bc4bc26c7652c439d54133ef0d318e6
PeachYuki <trylawen@gmail.com>
2025-03-25 12:35:13 +0000

    chore: scaffold project

---
+++ extended_hello_world/src/test/java/ch/bene/m223/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package ch.bene.m223;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
